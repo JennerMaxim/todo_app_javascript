92a20e7d97c50d6571008e222c3ca04acb889458
JennerMaxim <jennersi1remaxim@gmail.com>
2023-06-08 15:08:03 +0300

    Adding events to buttons and adding the todo list to the front-end

--- index.js
+++ index.js
@@ -3,22 +3,45 @@ let content = document.getElementById("content");
 let footer = document.getElementById("footer");
 
 let headerdata = () => {
-  return header.innerHTML = `
+  return (header.innerHTML = `
   <div class="container">
     <h1 class="logo">TODO APP</h1>
   </div>
-  `;
+  `);
 };
 headerdata();
 
+// Content
+
+let checkcontent = `<input type="checkbox" id="checkid"> `;
+
+let todoList = [
+  {
+    id: 1,
+    check: checkcontent,
+    todo: "Coding",
+  },
+  {
+    id: 2,
+    check: checkcontent,
+    todo: "Cooking",
+  },
+];
+
 let contentdata = () => {
-  return (content.innerHTML = `
-  <div class="container">
-    <div id="todo" class="todo"></div>
-  </div>
-  `);
+  return (content.innerHTML = todoList
+    .map((x) => {
+      let { id, check, todo } = x;
+      return `
+      <div id="todo" class="todo"><span id="checkids">${check}</span><span>${id}.</span> ${todo}</div>
+    `;
+    })
+    .join(""));
 };
-contentdata();
+let addtodo = contentdata();
+let checkid = document.getElementById("checkids");
+console.log(checkid);
+
 let footerdata = () => {
   return (footer.innerHTML = `
   <div class="container">
@@ -26,53 +49,76 @@ let footerdata = () => {
       <input id="input" type="text" />
     </div>
     <div class="buttons">
-      <button id="add" class="add">Add Todo</button>
-      <button id="clear" class="clear">Clear Completed</button>
+      <button id="add" onclick="addTodo()" class="add">Add Todo</button>
+      <button id="clear" onclick="clearTodo()" class="clear">Clear Completed</button>
     </div>
-    <span id="left">left</span>
+    <span ><span id="left">0</span> left</span>
   </div>
   `);
 };
 footerdata();
+
+// Incriment the left Todos
+var addTodo = () => {};
+
+
+// Check Boxes
+// let checkBoxes = () => {
+//   if (checkid.checked === true) {
+//     console.log("Checkbox is checked");
+//   } else {
+//     console.log("Checkbox is not checked");
+//   }
+// };
+
+// Decriment the left Todos
+let clearTodo = () => {
+  if (checkid.checked === true) {
+    console.log(todoList[0].check);
+  }
+  // console.log("clear todo");
+};
+
+// Update the left Todos
+let update = () => {};
+
 let add = document.getElementById("add");
 let clear = document.getElementById("clear");
 let id = 0;
 let todoss = [];
 // let todos;
 
-let check = `<input id="checkid" type="checkbox"> `;
-
 // Add Todo List
-add.addEventListener("click", () => {
-  id++;
-  let todo = document.getElementById("todo");
-  let inputTodo = document.getElementById("input").value;
+// add.addEventListener("click", () => {
+//   id++;
+//   let todo = document.getElementById("todo");
+//   let inputTodo = document.getElementById("input").value;
 
-  // Try
-  // let todocontent = todo.innerHTML = inputTodo
+//   // Try
+//   // let todocontent = todo.innerHTML = inputTodo
 
-  todo.innerHTML += check + id + ". " + inputTodo + "<br>";
-  todoss.push(id, inputTodo);
-  localStorage.setItem("data", content);
+//   todo.innerHTML += check + id + ". " + inputTodo + "<br>";
+//   todoss.push(id, inputTodo);
+//   JSON.stringify(localStorage.setItem("data", content));
 
-  console.log(todo);
-  return todoss;
-});
+//   console.log(todo);
+//   return todoss;
+// });
 
-let todos = localStorage.getItem("data");
+// let todos = JSON.parse(localStorage.getItem("data"));
 
-todo.innerHTML = todos;
-console.log(todos);
+// todo.innerHTML = todos;
+// console.log(todos);
 
 // Clear Todo List
-clear.addEventListener("click", () => {
-  let checkid = document.getElementById("checkid");
-  if (checkid === true) {
-    console.log("Hi");
-  } else {
-    console.log(todoss);
-  }
-});
+// clear.addEventListener("click", () => {
+//   let checkid = document.getElementById("checkid");
+//   if (checkid === true) {
+//     console.log("Hi");
+//   } else {
+//     console.log(todoss);
+//   }
+// });
 
 // let func1 = () => {
 //   return (fun3 = () => {
